0d8476cb281d936e507cbb849e1cd6e748329488
Richard Peterson <webbuyrich@gmail.com>
2016-01-18 02:26:55 -0600

    added syntax for ajax calls

--- js/script.js
+++ js/script.js
@@ -15,3 +15,24 @@ function goBack(){
 	$('#ajax').hide();
 	$('#call-success').show();
 }
+
+//sample ajax call 
+var url = '/employees.php';
+var data = { 
+	firstname: "Dave", 
+	lastname: "Johnson"
+}
+ var callback = function(response){
+
+ }
+
+ 
+
+ //more common syntax call
+ $.get('/employees.php', 
+ 	{ 
+ 		firstname: "Dave", 
+ 		lastname: "Johnson"
+ 	}, function(response){
+
+ });
